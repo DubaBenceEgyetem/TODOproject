ca6dc4d332ca7e88223e7611dc6cbed16204340f
Bence <bence22@gmail.com>
2024-03-18 22:32:32 +0100

    push

--- TODOApp/src/TODO.jsx
+++ TODOApp/src/TODO.jsx
@@ -1,14 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import Aos from "aos";
+import 'aos/dist/aos.css'
 
-function TODO({random, task, deleteTodo, editTodo }) {
+
+function TODO({ task, deleteTodo, editTodo }) {
+
+
+  
+  useEffect(() =>{
+    Aos.init({duration: 1000})
+}, [])
 
   return (
-    <div
+    <div  data-aos="fade-right"
       className={`bg-gradient-to-r from-random-color_2-primary to-random-color_2-secondary w-3/4 my-3 p-3 rounded-lg flex items-center justify-between`}
     >
-      <p className="text-3xl font-bold">{task.task}</p>
+      <p className="text-xs sm:text-3xl font-bold">{task.task}</p>
       <div className="space-x-5">
         <FontAwesomeIcon
           className="cursor-pointer"
