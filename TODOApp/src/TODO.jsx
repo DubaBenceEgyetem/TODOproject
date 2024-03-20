128e1366cc423a6340d7a361771d15f2d6058182
Duba Bence <duba.bence22@gmail.com>
2024-03-20 09:33:20 +0100

    push

--- TODOApp/src/TODO.jsx
+++ TODOApp/src/TODO.jsx
@@ -2,14 +2,30 @@ import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 
-function TODO({random, task, deleteTodo, editTodo }) {
+
+
+
+
+
+function TODO({ task, deleteTodo, editTodo }) {
+  
+  const [padding, setPadding] = useState(false)
+
+  function  handleClick(){
+    setPadding(!padding)
+  }
+
+
 
   return (
     <div
-      className={`bg-gradient-to-r from-random-color_2-primary to-random-color_2-secondary w-3/4 my-3 p-3 rounded-lg flex items-center justify-between`}
+      className={`bg-gradient-to-r from-random-color_2-primary gap-5 to-random-color_2-secondary w-3/4 my-3 p-3 rounded-lg flex  items-center`}
     >
-      <p className="text-3xl font-bold">{task.task}</p>
-      <div className="space-x-5">
+
+      <input type="checkbox" className="size-4" onClick={handleClick}></input>
+      <p className={`text-3xl font-bold ${padding ? ' text-gray-300 ease-in-out duration-300 px-2 line-through' : 'ease-in-out duration-300 '}`}>{task.task}</p>
+      <div className="flex-grow"></div> 
+      <div className="space-x-5  ml-auto">
         <FontAwesomeIcon
           className="cursor-pointer"
           icon={faTrash}
